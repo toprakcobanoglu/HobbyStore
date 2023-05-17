0e16a8527dac385e106b0e997b6efba305c464da
Toprak <toprakcobanoglu1@gmail.com>
2023-05-17 17:01:15 +0300

    adding files

--- src/main/resources/static/script/addToCart.js
+++ src/main/resources/static/script/addToCart.js
@@ -1,61 +1,33 @@
-// Sepetteki ürünleri depolamak için bir dizi tanımlayalım
-var sepet = [];
+function sepeteEkle(productId, name, basePrice) {
+    // Ürün bilgilerini sepet formuna eklemek için gerekli işlemleri yapın
+    var sepetFormu = document.getElementById("sepetFormu").getElementsByTagName("tbody")[0];
 
-// Ürünü sepete ekleyen fonksiyon
-function sepeteEkle(urunId, urunAdi, urunFiyati) {
-    // Ürünü temsil eden yeni bir nesne oluşturalım
-    var urun = {
-        id: urunId,
-        ad: urunAdi,
-        fiyat: parseFloat(urunFiyati) // Fiyatı sayısal değere dönüştürelim
+    var newRow = sepetFormu.insertRow();
+
+    var productIdCell = newRow.insertCell();
+    productIdCell.innerHTML = productId;
+
+    var nameCell = newRow.insertCell();
+    nameCell.innerHTML = name;
+
+    var priceCell = newRow.insertCell();
+    priceCell.innerHTML = basePrice;
+
+    updateTotalPrice();
+
+    var data = {
+        productId: productId,
+        name: name,
+        basePrice: basePrice
     };
 
-    // Ürünü sepete ekleyelim
-    sepet.push(urun);
-
-    // Sepeti güncelleyelim
-    guncelleSepet();
-}
-
-// Sepeti güncelleyen fonksiyon
-function guncelleSepet() {
-    // Sepeti temsil eden HTML tablosunu seçelim
-    var sepetTablosu = document.getElementById("sepetFormu");
-
-    // Sepetin içeriğini temizleyelim
-    sepetTablosu.innerHTML = "";
-
-    // Sepetteki her ürün için döngü oluşturalım
-    var toplamFiyat = 0;
-    for (var i = 0; i < sepet.length; i++) {
-        var urun = sepet[i];
-
-        // Ürünü temsil eden bir satır oluşturalım
-        var satir = document.createElement("tr");
-
-        // Ürün bilgilerini içeren hücreler oluşturalım
-        var urunIdHucresi = document.createElement("td");
-        urunIdHucresi.textContent = urun.id;
-
-        var urunAdiHucresi = document.createElement("td");
-        urunAdiHucresi.textContent = urun.ad;
-
-        var urunFiyatiHucresi = document.createElement("td");
-        urunFiyatiHucresi.textContent = urun.fiyat.toFixed(2); // Fiyatı 2 ondalık basamağa formatlayarak yazdıralım
-
-        // Satıra hücreleri ekleyelim
-        satir.appendChild(urunIdHucresi);
-        satir.appendChild(urunAdiHucresi);
-        satir.appendChild(urunFiyatiHucresi);
-
-        // Satırı sepet tablosuna ekleyelim
-        sepetTablosu.appendChild(satir);
-
-        // Toplam fiyatı güncelleyelim
-        toplamFiyat += urun.fiyat;
-    }
-
-    // Toplam fiyatı yazdıralım
-    var toplamFiyatHucresi = document.getElementById("toplamFiyat");
-    toplamFiyatHucresi.textContent = toplamFiyat.toFixed(2);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/purchase/total", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.send(JSON.stringify(data));
 }
